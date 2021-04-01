2c80817572d65f1ad297a73e0c7acbde4873fbf7
Alessio Buccino <alejoe9187@gmail.com>
2021-04-01 10:19:21 +0200

    Add auto wiring for ASSY156 to RHD2164

--- probeinterface/wiring.py
+++ probeinterface/wiring.py
@@ -5,10 +5,20 @@ import numpy as np
 
 pathways = {
     # this is the neuronexus H32 with omnetics connected to the intantec RHD headstage
-    'H32>RHD': [
+    'H32>RHD2132': [
         16, 17, 18, 20, 21, 22, 31, 30, 29, 27, 26, 25, 24, 28, 23, 19,
         12, 8, 3, 7, 6, 5, 4, 2, 1, 0, 9, 10, 11, 13, 14, 15],
+
+    'ASSY-156>RHD2164': [
+        17, 16, 19, 18, 21, 20, 23, 22, 25, 24, 27, 26, 29, 28, 31, 30,
+        33, 32, 35, 34, 37, 36, 39, 38, 41, 40, 43, 42, 45, 44, 47, 46,
+        49, 48, 51, 50, 53, 52, 55, 54, 57, 56, 59, 58, 61, 60, 63, 62,
+        1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14],
 }
+
+
+def get_available_pathways():
+    return list(pathways.keys())
 
 
 def wire_probe(probe, pathway, channel_offset=0):
