780ee12d97f04d28f5cccaee71cf208f3da6e782
Zach McKenzie <92116279+zm711@users.noreply.github.com>
2023-03-07 11:10:59 -0500

    Update ex_01_generate_probe_from_sratch.py
    fix typos.

--- examples/ex_01_generate_probe_from_sratch.py
+++ examples/ex_01_generate_probe_from_sratch.py
@@ -14,7 +14,7 @@ from probeinterface import Probe
 from probeinterface.plotting import plot_probe
 
 ##############################################################################
-# First, let's create dummy positions for a 32-contact probe
+# First, let's create dummy positions for a 24-contact probe
 
 n = 24
 positions = np.zeros((n, 2))
@@ -30,7 +30,7 @@ positions[8:16, 1] -= 10
 #  and set the position and shape of each contact
 #  
 # The `ndim` argument indicates that the contact is 2d, so the positions have a (n_elec, 2) shape.
-#  We can also define 3d probe with `ndim=3` and positions will have a (n_elec, 3) shape.
+#  We can also define a 3d probe with `ndim=3` and positions will have a (n_elec, 3) shape.
 #  
 # Note: `shapes` and `shape_params` could be arrays as well, indicating the shape for each contact separately.
 
@@ -43,7 +43,7 @@ probe.set_contacts(positions=positions, shapes='circle', shape_params={'radius':
 print(probe)
 
 ##############################################################################
-# In addition to contacts, we can crate the planar contour (polygon) of the probe
+# In addition to contacts, we can create the planar contour (polygon) of the probe
 
 polygon = [(-20, -30), (20, -110), (60, -30), (60, 190), (-20, 190)]
 probe.set_planar_contour(polygon)
@@ -60,7 +60,7 @@ df
 plot_probe(probe)
 
 ##############################################################################
-# A 2d `Probe` can be transformed to a 3d `Probe` by indicating the `axes`
+# A 2d `Probe` can be transformed into a 3d `Probe` by indicating the `axes`
 # on which contacts will lie (Here the 'y' coordinate will be 0 for all contacts):
 
 probe_3d = probe.to_3d(axes='xz')
