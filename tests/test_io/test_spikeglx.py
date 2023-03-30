244749b53f0a5637468682c19e555c0031846ab9
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-03-30 18:21:00 +0200

    refactor and rename tests

--- tests/test_io/test_spikeglx.py
+++ tests/test_io/test_spikeglx.py
@@ -11,16 +11,30 @@ from probeinterface import (
 
 data_path = Path(__file__).absolute().parent.parent / "data" / "spikeglx"
 
+def test_parse_meta():
+    for meta_file in [
+        "doppio-checkerboard_t0.imec0.ap.meta",
+        "NP1_saved_only_subset_of_channels.meta",
+        "allan-longcol_g0_t0.imec0.ap.meta",
+    ]:
+        meta = parse_spikeglx_meta(data_path / meta_file)
+
+def test_get_saved_channel_indices_from_spikeglx_meta():
+    # all channel saved + 1 synchro
+    chan_inds = get_saved_channel_indices_from_spikeglx_meta(
+        data_path / "Noise_g0_t0.imec0.ap.meta"
+    )
+    assert chan_inds.size == 385
+
+    # example by Pierre Yger NP1.0 with 384 but only 151 channels are saved + 1 synchro
+    chan_inds = get_saved_channel_indices_from_spikeglx_meta(
+        data_path / "NP1_saved_only_subset_of_channels.meta"
+    )
+    assert chan_inds.size == 152
 
 def test_NP1():
     probe = read_spikeglx(data_path / "Noise_g0_t0.imec0.ap.meta")
     assert "1.0" in probe.annotations["name"]
-
-
-def test_NP2_4_shanks():
-    probe = read_spikeglx(data_path / "TEST_20210920_0_g0_t0.imec0.ap.meta")
-    assert probe.get_shank_count() == 4
-    assert "2.0" in probe.annotations["name"]
 
 
 def test_NP2_1_shanks():
@@ -31,19 +45,67 @@ def test_NP2_1_shanks():
 
 def test_NP_phase3A():
     # Data provided by rtraghavan
-    probe = read_spikeglx(data_path / "NeuropixelPhase3A_file_g0_t0.imec.ap.meta")
-    assert "Phase3a" in probe.annotations["name"]
-    assert probe.get_shank_count() == 1
+    probe = read_spikeglx(data_path / "phase3a.imec.ap.meta")
 
+    assert probe.annotations["name"] == "Phase3a"
+    assert probe.annotations["manufacturer"] == "IMEC"
+    assert probe.annotations["probe_type"] == "Phase3a"
+
+    assert probe.ndim == 2
+    assert probe.get_shank_count() == 1
+    assert probe.get_contact_count() == 384
+    
+    # Test contact geometry
+    contact_width = 12.0
+    contact_shape = "square"
+
+    assert np.all(probe.contact_shape_params == {"width": contact_width})
+    assert np.all(probe.contact_shapes == contact_shape)
 
 def test_NP2_4_shanks():
-    # Data provided by Jennifer Colonell
-    probe = read_spikeglx(data_path / "NP24_g0_t0.imec0.ap.meta")
-    assert "2.0" in probe.annotations["name"]
+    probe = read_spikeglx(data_path / "NP2_4_shanks.imec0.ap.meta")
+
+    assert probe.annotations["name"] == "Neuropixels 2.0 - Four Shank"
+    assert probe.annotations["manufacturer"] == "IMEC"
+    assert probe.annotations["probe_type"] == 24
+    
+    assert probe.ndim == 2
     assert probe.get_shank_count() == 4
+    assert probe.get_contact_count() == 384
+    
+    
+    # Test contact geometry
+    contact_width = 12.0
+    contact_shape = "square"
 
+    assert np.all(probe.contact_shape_params == {"width": contact_width})
+    assert np.all(probe.contact_shapes == contact_shape)
 
-def test_NP1_large_depth_sapn():
+def test_NP2_4_shanks_with_different_electrodes_saved():
+    # Data provided by Jennifer Colonell
+    probe = read_spikeglx(data_path / "NP2_4_shanks_save_different_electrodes.imec0.ap.meta")
+
+    assert probe.annotations["name"] == "Neuropixels 2.0 - Four Shank"
+    assert probe.annotations["manufacturer"] == "IMEC"
+    assert probe.annotations["probe_type"] == 24
+    
+    assert probe.ndim == 2
+    assert probe.get_shank_count() == 4
+    assert probe.get_contact_count() == 384
+    
+    # Test contact geometry
+    contact_width = 12.0
+    contact_shape = "square"
+
+    assert np.all(probe.contact_shape_params == {"width": contact_width})
+    assert np.all(probe.contact_shapes == contact_shape)
+    
+    # This file does not save the channnels from 0 as the one above (NP2_4_shanks_g0_t0.imec0.ap.meta)
+    ypos = probe.contact_positions[:, 1]
+    assert np.min(ypos) == pytest.approx(4080.0)
+
+        
+def test_NP1_large_depth_span():
     # Data provided by Tom Bugnon NP1 with large Depth span
     probe = read_spikeglx(data_path / "allan-longcol_g0_t0.imec0.ap.meta")
     assert "1.0" in probe.annotations["name"]
@@ -64,34 +126,10 @@ def test_NP1_other_example():
 
 def tes_NP1_384_channels():
     # example by Pierre Yger NP1.0 with 384 but only 151 channels are saved
-    probe = read_spikeglx(data_path / "Day_3_g0_t0.imec1.ap.meta")
+    probe = read_spikeglx(data_path / "NP1_saved_only_subset_of_channels.meta")
     assert probe.get_shank_count() == 1
     assert probe.get_contact_count() == 151
     assert 152 not in probe.contact_annotations["channel_ids"]
-
-
-def test_parse_meta():
-    for meta_file in [
-        "doppio-checkerboard_t0.imec0.ap.meta",
-        "Day_3_g0_t0.imec1.ap.meta",
-        "allan-longcol_g0_t0.imec0.ap.meta",
-    ]:
-        meta = parse_spikeglx_meta(data_path / meta_file)
-
-
-def test_get_saved_channel_indices_from_spikeglx_meta():
-    # all channel saved + 1 synchro
-    chan_inds = get_saved_channel_indices_from_spikeglx_meta(
-        data_path / "Noise_g0_t0.imec0.ap.meta"
-    )
-    assert chan_inds.size == 385
-
-    # example by Pierre Yger NP1.0 with 384 but only 151 channels are saved + 1 synchro
-    chan_inds = get_saved_channel_indices_from_spikeglx_meta(
-        data_path / "Day_3_g0_t0.imec1.ap.meta"
-    )
-    assert chan_inds.size == 152
-
 
 def test_NPH_long_staggered():
     # Data provided by Nate Dolensek
