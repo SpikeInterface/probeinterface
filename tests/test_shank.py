4cae230df2486e65cf55e61da6bae108f149cbd8
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-04-01 14:15:48 +0200

    correct keyword

--- tests/test_shank.py
+++ tests/test_shank.py
@@ -9,12 +9,12 @@ def testing_shank():
     num_shank = 1
     num_columns = 1
     num_contact_per_column = 6
-    elec_shapes = "square"
+    contact_shapes = "square"
     multi_shank_probe = generate_multi_shank(
         num_shank=num_shank,
         num_columns=num_columns,
         num_contact_per_column=num_contact_per_column,
-        elec_shapes=elec_shapes,
+        contact_shapes=contact_shapes,
     )
     shank = multi_shank_probe.get_shanks()[0]
 
