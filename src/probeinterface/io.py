1bfac0ebc7dabb0ca6cb7ab1a60d91577faf89d2
Alessio Buccino <alejoe9187@gmail.com>
2023-12-05 11:40:00 +0100

    Add missing NP2.4 probe part number

--- src/probeinterface/io.py
+++ src/probeinterface/io.py
@@ -1026,6 +1026,7 @@ probe_part_number_to_probe_type = {
     "NP2003": "2003",
     "NP2004": "2004",
     "PRB2_1_2_0640_0": "21",
+    "PRB2_4_2_0640_0": "24",
     # Other probes
     "NP1100": "1100",  # Ultra probe - 1 bank
     "NP1110": "1100",  # Ultra probe - 16 banks
