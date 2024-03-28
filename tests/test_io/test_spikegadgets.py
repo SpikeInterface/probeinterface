4ce4791e6d7e557e409d7bd4b44fb3db9350be63
Greg Knoll <gregory@bccn-berlin.de>
2024-03-28 16:43:07 +0100

    Update imports in test to access internal function.

--- tests/test_io/test_spikegadgets.py
+++ tests/test_io/test_spikegadgets.py
@@ -3,10 +3,8 @@ from xml.etree import ElementTree
 
 import pytest
 
-from probeinterface import (
-    read_spikegadgets,
-    parse_spikegadgets_header,
-)
+from probeinterface import read_spikegadgets
+from probeinterface.io import parse_spikegadgets_header
 
 data_path = Path(__file__).absolute().parent.parent / "data" / "spikegadgets"
 test_file = "SpikeGadgets_test_data_2xNpix1.0_20240318_173658_header_only.rec"
