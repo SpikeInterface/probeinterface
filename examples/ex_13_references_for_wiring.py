a8efd77f59c4ea07ae8f08174e3b20a22c38d8fc
chrishalcrow <57948917+chrishalcrow@users.noreply.github.com>
2025-01-20 13:42:10 +0000

    Add one wiring reference doc

---
+++ examples/ex_13_references_for_wiring.py
@@ -0,0 +1,29 @@
+"""
+Reference guide for wirings
+---------------------------
+
+Probeinterface includes some commonly used wirings, which link the probe sites to
+the signal order in raw data files. Here, we keep a reference guide so that users 
+can check these wirings for inaccuracies. For each wiring we keep a `.xlsx` file with
+all intermediate steps of the wiring process and a `.pdf` containing all figures and 
+links needed to reproduce the result.
+
+The final columns in the `.xlsx` file should match the wiring arrays in 
+`wiring.py`, `here <https://github.com/SpikeInterface/probeinterface/blob/48d759bcbda23b0cf6c222f4c18c6433bf818c8b/src/probeinterface/wiring.py>`_.
+
+.. list-table:: 
+   :widths: 50 25 25
+   :header-rows: 1
+
+   * - Wiring 
+     - .pdf
+     - .xls
+   * - H32>RHD2132
+     -
+     - 
+   * - ASSY-77>Adpt.A64-Om32_2x-sm-NN>RHD2164
+     - `.pdf <../../img/wiring_references/ASSY-77\>Adpt.A64-Om32_2x-sm-NN\>RHD2164.pdf>`_
+     - `.xlsx <../../img/wiring_references/ASSY-77\>Adpt.A64-Om32_2x-sm-NN\>RHD2164.xlsx>`_
+
+
+"""
