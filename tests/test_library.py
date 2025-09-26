11ddb960871803d1d76c5be025df2036be5c0e8d
Alessio Buccino <alejoe9187@gmail.com>
2025-09-26 16:46:32 +0200

    Add tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,5 +1,12 @@
 from probeinterface import Probe
-from probeinterface.library import download_probeinterface_file, get_from_cache, get_probe
+from probeinterface.library import (
+    download_probeinterface_file,
+    get_from_cache,
+    get_probe,
+    get_tags_in_library,
+    get_manufacturers_in_library,
+    get_probes_in_library,
+)
 
 
 from pathlib import Path
@@ -31,7 +38,33 @@ def test_get_probe():
     assert probe.get_contact_count() == 32
 
 
+def test_available_tags():
+    tags = get_tags_in_library()
+    if len(tags) > 0:
+        for tag in tags:
+            assert isinstance(tag, str)
+            assert len(tag) > 0
+
+
+def test_get_manufacturers_in_library():
+    manufacturers = get_manufacturers_in_library()
+    assert isinstance(manufacturers, list)
+    assert "neuronexus" in manufacturers
+    assert "imec" in manufacturers
+
+
+def test_get_probes_in_library():
+    manufacturers = get_manufacturers_in_library()
+    for manufacturer in manufacturers:
+        probes = get_probes_in_library(manufacturer)
+        assert isinstance(probes, list)
+        assert len(probes) > 0
+
+
 if __name__ == "__main__":
     test_download_probeinterface_file()
     test_get_from_cache()
     test_get_probe()
+    test_get_latest_tag()
+    test_get_manufacturers_in_library()
+    test_get_probes_in_library()
